e2e378b0bafed9d5d8240f506c3aced42c5125a1
ombahiwal <ombahiwal@gmail.com>
2019-04-13 02:21:13 +0530

    change footer https

--- footer.php
+++ footer.php
@@ -4,24 +4,24 @@
 				<div class="col-xs-12 col-sm-4 col-md-4">
 					<h5 id="ft">Quick links</h5>
 					<ul class="list-unstyled quick-links">
-						<li><a href="http://tedtech.in"><i class="fa fa-angle-double-right"></i>Home</a></li>
-						<li><a href="http://tedtech.in/about.php"><i class="fa fa-angle-double-right"></i>About</a></li>
-						<li><a href="http://tedtech.in/about.php#contact"><i class="fa fa-angle-double-right"></i>Contact Us</a></li>
+						<li><a href="https://tedtech.in"><i class="fa fa-angle-double-right"></i>Home</a></li>
+						<li><a href="https://tedtech.in/about.php"><i class="fa fa-angle-double-right"></i>About</a></li>
+						<li><a href="https://tedtech.in/about.php#contact"><i class="fa fa-angle-double-right"></i>Contact Us</a></li>
 					</ul>
 				</div>
 				<div class="col-xs-12 col-sm-4 col-md-4">
 					<h5 id="ft">Our Services</h5>
 					<ul class="list-unstyled quick-links">
-						<li><a href="http://tedtech.in/developer/panel/index.php"><i class="fa fa-angle-double-right"></i>Develop Projects</a></li>
-						<li><a href="http://tedtech.in/uploader/panel/login.php"><i class="fa fa-angle-double-right"></i>Upload Projects</a></li>
+						<li><a href="https://tedtech.in/developer/panel/index.php"><i class="fa fa-angle-double-right"></i>Develop Projects</a></li>
+						<li><a href="https://tedtech.in/uploader/panel/login.php"><i class="fa fa-angle-double-right"></i>Upload Projects</a></li>
 					</ul>
 				</div>
 				<div class="col-xs-12 col-sm-4 col-md-4">
 					<h5 id="ft">Legal Links</h5>
 					<ul class="list-unstyled quick-links">
-						<li><a href="http://tedtech.in/docs/TC.pdf" target="_blank"><i class="fa fa-angle-double-right" ></i>Terms and Conditions</a></li>
-						<li><a href="http://tedtech.in/docs/PRIVACY.pdf"><i class="fa fa-angle-double-right" target="_blank"></i>Privacy Policy</a></li>
-						<li><a href="http://tedtech.in/docs/SR.pdf" target="_blank"><i class="fa fa-angle-double-right"></i>Shipping and Return Policy</a></li>
+						<li><a href="https://tedtech.in/docs/TC.pdf" target="_blank"><i class="fa fa-angle-double-right" ></i>Terms and Conditions</a></li>
+						<li><a href="https://tedtech.in/docs/PRIVACY.pdf"><i class="fa fa-angle-double-right" target="_blank"></i>Privacy Policy</a></li>
+						<li><a href="https://tedtech.in/docs/SR.pdf" target="_blank"><i class="fa fa-angle-double-right"></i>Shipping and Return Policy</a></li>
 						
 					</ul>
 				</div>
@@ -41,7 +41,7 @@
 				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
 					<ul class="list-unstyled list-inline social text-center">
 						<li class="list-inline-item"><table border="0" cellpadding="10" cellspacing="0" align="center"><tr><td align="center"></td></tr><tr><td align="center"><a href="https://www.paypal.com/webapps/mpp/paypal-popup" title="How PayPal Works" onclick="javascript:window.open('https://www.paypal.com/webapps/mpp/paypal-popup','WIPaypal','toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=1060, height=700'); return false;"><img src="https://www.paypalobjects.com/digitalassets/c/website/marketing/na/us/logo-center/9_bdg_secured_by_pp_2line.png" border="0" alt="Secured by PayPal"></a></td><td><span id="siteseal"><script async type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=wBzXsJr3uYr5Fgv4R6u1K9bSZULf545sIcsryTgqbCpdkguase1DYKd5wEPi"></script></span></td><td>
-                            <img src="http://tedtech.in/images/footer/aws.png">
+                            <img src="https://tedtech.in/images/footer/aws.png">
                         
                             </td></tr><tr></table></li>
 					</ul>
